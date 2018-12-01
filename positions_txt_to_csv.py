cf0ed82a09b2ddd4cfca69a44a3a9f4ea9c5a758
VadimFarutin <vfarut@gmail.com>
2018-12-02 00:34:52 +0300

    Added tests

---
+++ positions_txt_to_csv.py
@@ -0,0 +1,29 @@
+import sys
+import csv
+import numpy as np
+
+
+def convert_positions(test_path):
+    with open(test_path + '/result_object_animation.txt',
+              newline='') as csv_input, \
+         open(test_path + '/positions.csv', 'w',
+              newline='') as csv_output:
+
+        params_reader = csv.reader(csv_input, delimiter=' ')
+        params_writer = csv.writer(csv_output, delimiter=',')
+        index = 0
+
+        for row in params_reader:
+            indexed_row = np.concatenate(([index], [float(x) for x in row]),
+                                         axis=None)
+            params_writer.writerow(indexed_row)
+            index += 1
+
+
+if __name__ == '__main__':
+    args = sys.argv
+    if len(args) != 2:
+        print('Wrong number of arguments!')
+        print('Expected format: python positions_txt_to_csv.py <test_path>')
+        exit(1)
+    convert_positions(args[1])
