2b73b3e8e2827f3d059800e04e883da5e0c1df44
VadimFarutin <vfarut@gmail.com>
2019-01-24 17:57:01 +0300

    Moved method into a converter class

--- positions_txt_to_csv.py
+++ positions_txt_to_csv.py
@@ -3,21 +3,23 @@ import csv
 import numpy as np
 
 
-def convert_positions(test_path):
-    with open(test_path + '/result_object_animation.txt',
-              newline='') as csv_input, \
-         open(test_path + '/positions.csv', 'w',
-              newline='') as csv_output:
+class Converter:
+    @staticmethod
+    def convert_positions(test_path):
+        with open(test_path + '/result_object_animation.txt',
+                  newline='') as csv_input, \
+             open(test_path + '/positions.csv', 'w',
+                  newline='') as csv_output:
 
-        params_reader = csv.reader(csv_input, delimiter=' ')
-        params_writer = csv.writer(csv_output, delimiter=',')
-        index = 0
+            params_reader = csv.reader(csv_input, delimiter=' ')
+            params_writer = csv.writer(csv_output, delimiter=',')
+            index = 0
 
-        for row in params_reader:
-            indexed_row = np.concatenate(([index], [float(x) for x in row]),
-                                         axis=None)
-            params_writer.writerow(indexed_row)
-            index += 1
+            for row in params_reader:
+                indexed_row = np.concatenate(([index], [float(x) for x in row]),
+                                             axis=None)
+                params_writer.writerow(indexed_row)
+                index += 1
 
 
 if __name__ == '__main__':
@@ -26,4 +28,5 @@ if __name__ == '__main__':
         print('Wrong number of arguments!')
         print('Expected format: python positions_txt_to_csv.py <test_path>')
         exit(1)
-    convert_positions(args[1])
+
+    Converter.convert_positions(args[1])
